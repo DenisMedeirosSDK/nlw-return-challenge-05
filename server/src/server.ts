779fba0b20e99989eb2ff2cb7aee4ab01076eccd
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2022-05-08 09:21:30 -0300

    remove origin cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,11 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: ['https://nlw-return-olive.vercel.app/'],
-  })
-);
+app.use(cors());
 
 app.get('/', (request: Request, response: Response) => {
   return response.end('hello world');
